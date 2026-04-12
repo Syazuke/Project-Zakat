72ea94fc7f72dd4ac7c37b25a0c727291dad0ec3
sz <szzsyazukee@gmail.com>
2026-04-12 09:39:36 +0700

    Fix: Build sukses dan siap deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,41 @@
 /** @type {import('next').NextConfig} */
+
+// Kita buat Daftar VIP (Whitelist) di sini
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.sandbox.midtrans.com https://app.midtrans.com;
+  style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com;
+  font-src 'self' https://cdnjs.cloudflare.com;
+  img-src 'self' data: https:;
+  connect-src 'self' https://app.sandbox.midtrans.com https://app.midtrans.com;
+  frame-src 'self' https://app.sandbox.midtrans.com https://app.midtrans.com;
+`
+  .replace(/\s{2,}/g, " ")
+  .trim();
+
 const nextConfig = {
   async headers() {
     return [
       {
+        // Terapkan pelindung ini ke seluruh halaman website /(.*)
         source: "/(.*)",
         headers: [
-          { key: "X-XSS-Protection", value: "1; mode=block" },
-          { key: "X-Frame-Options", value: "DENY" }, // Mencegah web Anda ditempel di web penipu (Clickjacking)
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader,
+          },
+          {
+            key: "X-Frame-Options", // Mencegah web Anda di-copy ke dalam iframe (Clickjacking)
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options", // Mencegah browser menebak-nebak tipe file
+            value: "nosniff",
+          },
+          {
+            key: "Strict-Transport-Security", // Memaksa selalu pakai HTTPS
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
         ],
       },
     ];
