0164cbec5b158db346d918eb6639cb0a8c64fa88
Syazuke <ssyazukee@gmail.com>
2026-05-01 16:53:08 +0700

    update pembayaran

--- app/components/atoms/Zakat.jsx
+++ app/components/atoms/Zakat.jsx
@@ -42,12 +42,6 @@ const Zakat = ({ nominalZakat, Type }) => {
       onClose: function () {
         setIsLoading(false);
       },
-      onCancel: function () {
-        alert("Pembayaran di cancel!");
-        setSnapToken(null);
-        setIsLoading(false);
-        setIsPending(false);
-      },
     });
   };
 
